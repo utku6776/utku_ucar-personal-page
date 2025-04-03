6a5826cc6322f6a5db4744ed15354b65f980a6b5
utku6776 <168765912+utku6776@users.noreply.github.com>
2025-04-03 21:51:30 +0300

    yeni guncelleme
    yeni tasarima gecildi

--- script.js
+++ script.js
@@ -1,55 +1,54 @@
-function scrollToElement(elementSelector,instance=0){
-    const elements = document.querySelectorAll(elementSelector);
-    if(elements.length > instance) { 
-        elements[instance].scrollIntoView({behavior:'smooth'});
+document.addEventListener("DOMContentLoaded", function () {
+    const mobBtn = document.getElementById("mob-btn");
+    const navMob = document.getElementById("nav-mob");
 
+    mobBtn.addEventListener("click", function () {
+        navMob.classList.toggle("hidden"); // Menüyü aç/kapat
+    });
+});
+
+
+
+function scrollToElementById(elementId) {
+    const element = document.getElementById(elementId);
+    if (element) { 
+        element.scrollIntoView({ behavior: 'smooth' });
     }
 }
 
-const link1 = document.getElementById("link1")
-const link2 = document.getElementById("link2")
-const link3 = document.getElementById("link3")
-const link4 = document.getElementById("link4")
-const up = document.getElementById("up")
 
-link1.addEventListener('click',() =>{
-    scrollToElement(".contentme")
-})
-
-link2.addEventListener('click',() =>{
-    scrollToElement(".content-feature")
-})
-
-link3.addEventListener('click',() =>{
-    scrollToElement(".plan")
-})
-
-link4.addEventListener('click',() =>{
-    scrollToElement(".container-comm")
-})
-
-up.addEventListener('click',() =>{
-    scrollToElement(".home");
-})
+const link1 = document.getElementById("link1");
+const link2 = document.getElementById("link2");
+const link3 = document.getElementById("link3");
+const link4 = document.getElementById("link4");
+const link5 = document.getElementById("link5");
+const link6 = document.getElementById("link6");
 
 
-const nav_but = document.getElementById("nav-but")
-const nav_back = document.getElementById("nav-back")
-const nav = document.getElementById("navbar");
+link1.addEventListener('click', () => {
+    scrollToElementById("about"); 
+});
 
-nav_back.style.display = "none"
+link2.addEventListener('click', () => {
+    scrollToElementById("features"); 
+});
 
-
-nav_but.addEventListener('click',function(){
-    
-    nav.style.width = "170px";
-    nav_but.style.display = "none"
-    nav_back.style.display = "block"
-})
+link3.addEventListener('click', () => {
+    scrollToElementById("communication"); 
+});
 
 
-nav_back.addEventListener("click",function(){
-    nav.style.width = "0px"
-    nav_but.style.display = "block"
-    nav_back.style.display = "none"
-})
+
+
+link4.addEventListener('click', () => {
+    scrollToElementById("about"); 
+});
+
+link5.addEventListener('click', () => {
+    scrollToElementById("features"); 
+});
+
+
+link6.addEventListener('click', () => {
+    scrollToElementById("communication"); 
+});
